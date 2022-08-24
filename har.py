33c917f32d2625876b515e7c0d309071d5d15fbd
despencer <despencer@mail.ru>
2022-08-24 22:30:14 +0300

    initial coding for make request.

--- har.py
+++ har.py
@@ -1,10 +1,39 @@
 import json
 
+class HttpCookie:
+    def __init__(self):
+        self.name = ''
+        self.value = ''
+        self.domain = ''
+        self.path = '/'
+        self.expires = None
+
 class HttpRequest:
     def __init__(self):
         self.method = 'GET'
         self.url = ''
         self.headers = {}
+
+class HttpResponce:
+    def __init__(self):
+        self.status = 0
+        self.text = ''
+        self.headers = {}
+        self.cookies = []
+
+    def setheaders(self, headers):
+        for n,v in headers.items():
+           self.headers[n]=v
+
+    def setcookies(self, cjar):
+        for c in cjar.values():
+            ck = HttpCookie()
+            ck.name = c.name
+            ck.value = c.value
+            ck.path = c.path
+            ck.domain = c.domain
+            ck.expires = c.expires
+            self.cookies.append(ck)
 
 def loadrequest(harfile):
     with open(harfile) as hfile:
@@ -16,3 +45,27 @@ def loadrequest(harfile):
         for jheader in jreq['headers']:
             req.headers[jheader['name']] = [jheader['value']]
     return req
+
+def savehttpheaders(headers):
+    jheaders = []
+    for name, value in headers.items():
+        jheaders.append( { 'name':name, 'value':value } )
+    return jheaders
+
+def savehttpcookies(cookies):
+    jcookies = []
+    for c in cookies:
+        jcookies.append( { 'name':c.name, 'value':c.value, 'domain':c.domain, 'path':c.path, 'expires':c.expires } )
+    return jcookies
+
+def savehttprequest(req):
+    return { 'method':req.method, 'url':req.url, 'headers':savehttpheaders(req.headers) }
+
+def savehttpresponce(resp):
+    return { 'status':resp.status, 'text':resp.text, 'headers':savehttpheaders(resp.headers), 'cookies':savehttpcookies(resp.cookies) }
+
+def saveresponce(harfile, request, responce):
+    jentry = { 'request' : savehttprequest(request), 'responce' : savehttpresponce(responce) }
+    with open(harfile, 'w') as jfile:
+        json.dump( { 'log':{ 'entries' : [ jentry ] } } , jfile, indent = 4 )
+
--- makereq.py
+++ makereq.py
@@ -4,9 +4,19 @@ import argparse
 import requests
 import har
 
+def save(responce, harfile):
+    resp = har.HttpResponce()
+    resp.status = responce.status_code
+    resp.text = responce.text
+    resp.setheaders(responce.headers)
+    resp.setcoockies(responce.cookies)
+    har.saveresponce(resp, harfile)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Make http request')
     parser.add_argument('request', help='HAR-file with request')
     parser.add_argument('responce', help='Target HAR-file with responce')
     args = parser.parse_args()
     req = har.loadrequest(args.request)
+    responce = requests.get(req.url, headers=headers, allow_redirects=False)
+    save(responce, args.responce)
