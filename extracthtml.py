c57287e44035ee9f8d1766574584f15f1e26cade
despencer <despencer@mail.ru>
2022-08-28 23:51:17 +0300

    added html parsing & beautifing.

--- extracthtml.py
+++ extracthtml.py
@@ -2,6 +2,8 @@
 
 import argparse
 import har
+import html5lib
+from xml.etree import ElementTree
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract html code')
@@ -9,5 +11,7 @@ if __name__ == '__main__':
     parser.add_argument('html', help='Target html-file')
     args = parser.parse_args()
     resp = har.loadresponce(args.responce)
+    document = html5lib.parse(resp.text)
+    ElementTree.indent(document)
     with open(args.html, 'w') as hfile:
-        hfile.write(resp.text)
+        hfile.write(ElementTree.tostring(document, encoding='unicode'))
