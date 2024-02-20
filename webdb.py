8cc982d8bcd5e189b604aeebc7616ad1d0fb7a23
despencer <despencer@mail.ru>
2024-02-20 15:17:59 +0300

    loading table was added.

--- webdb.py
+++ webdb.py
@@ -26,9 +26,18 @@ class UrlTask:
     def get(cls, db, id):
         return dbmeta.DbMeta.get(db, cls, id)
 
+class Loading:
+    def __init__(self):
+        dbmeta.DbMeta.init(Loading, self)
+
+    @classmethod
+    def create(cls, db, task, request, responce)
+
 def init(db):
     db.deploypacket('web',1,
         [ "CREATE TABLE web_url_task (id INTEGER NOT NULL, status TEXT NOT NULL, origin INTEGER NULL, kind TEXT NOT NULL, url TEXT NOT NULL, PRIMARY KEY(id))",
-          "CREATE VIEW web_tasks (id, kind, status) AS SELECT id, 'url', status FROM web_url_task" ] )
+          "CREATE VIEW web_tasks (id, kind, status) AS SELECT id, 'url', status FROM web_url_task",
+          "CREATE TABLE web_loading (id INTEGET NOT NULL, urltask INTEGER NOT NULL, request BLOB NOT NULL, responce BLOB NULL, PRIMARY KEY(id), FOREIGN KEY (url_task) REFERENCES web_url_task (id))" ] )
     dbmeta.DbMeta.set(Task, 'web_tasks', ['id', 'kind', 'status'])
     dbmeta.DbMeta.set(UrlTask, 'web_url_task', ['id', 'status', 'origin', 'kind', 'url'])
+    dbmeta.DbMeta.set(Loading, 'web_loading', ['id', 'urltask', 'request', 'responce'])
